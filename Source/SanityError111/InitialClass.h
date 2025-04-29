64f103f330897d1a13f2ecf32845cde67a7a7a35
leksi <skrip3151@gmail.com>
2025-04-29 21:34:40 +0200

    added c++ files to the project
    to make build work w plugin

---
+++ Source/SanityError111/InitialClass.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class SANITYERROR111_API InitialClass
+{
+public:
+	InitialClass();
+	~InitialClass();
+};
